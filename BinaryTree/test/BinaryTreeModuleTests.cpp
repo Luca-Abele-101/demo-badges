71dcde4538af70ed22bbcad61a43fab73aede4db
Pavel Luchshiy <luca-abele-101@yandex.ru>
2025-08-16 16:26:27 +0300

    Test badges

--- BinaryTree/test/BinaryTreeModuleTests.cpp
+++ BinaryTree/test/BinaryTreeModuleTests.cpp
@@ -4,7 +4,7 @@
 TEST(binTree_tests_case, isEmpty_test)
 {
     BinarySearchTree<int> sus;
-    EXPECT_EQ(sus.isEmpty(), 0);
+    EXPECT_EQ(sus.isEmpty(), 1);
 }
 
 TEST(binTree_tests_case, isNotEmpty_test)
